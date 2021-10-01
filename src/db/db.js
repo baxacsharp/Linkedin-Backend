f8413c1be8c1c9b25bc51bbe811e34345453daa3
baxacsharp <78537820+baxacsharp@users.noreply.github.com>
2021-10-01 11:31:02 +0200

    pool removed

--- src/db/db.js
+++ src/db/db.js
@@ -5,25 +5,10 @@ import { Pool } from "pg"
 const { PGUSER, PGPORT, PGDATABASE, PGPASSWORD, DATABASE_URL } = process.env
 const pool = new Pool()
 
-pool.on("error", (err, client) => {
-  console.error("Unexpected error on client", err)
-  process.exit(-1)
-})
-pool.on("connect", (err, client) => {
-  if (err) console.error(err)
-  console.log(client)
-  console.log("Successfully connected to postgress")
-})
 const sequelize = new Sequelize(DATABASE_URL, {
   port: PGPORT,
   host: "https://linkedin-sql.herokuapp.com",
   dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
 })
 
 sequelize
